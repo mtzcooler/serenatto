1f5ce75d0523dcb930183647a4fbe4ef04bc16d7
Matheus Zoccoler <mtzcooler@gmail.com>
2025-04-02 17:16:19 +0100

    Connection with database using PDO

--- index.php
+++ index.php
@@ -1,57 +1,10 @@
 <?php
-$produtosCafe = [
-    [
-        'nome' => 'Café Cremoso',
-        'descricao' => 'Café cremoso irresistivelmente suave e que envolve seu paladar',
-        'preco' => 5.00,
-        'imagem' => 'img/cafe-cremoso.jpg'
-    ],
-    [
-        'nome' => 'Café com Leite',
-        'descricao' => 'A harmonia perfeita do café e do leite, uma experiência reconfortante',
-        'preco' => 2.00,
-        'imagem' => 'img/cafe-com-leite.jpg'
-    ],
-    [
-        'nome' => 'Cappuccino',
-        'descricao' => 'Café suave, leite cremoso e uma pitada de sabor adocicado',
-        'preco' => 7.00,
-        'imagem' => 'img/cappuccino.jpg'
-    ],
-    [
-        'nome' => 'Café Gelado',
-        'descricao' => 'Café gelado refrescante, adoçado e com notas sutis de baunilha ou caramelo.',
-        'preco' => 4.00,
-        'imagem' => 'img/cafe-gelado.jpg'
-    ]
-];
 
-$produtosAlmoco = [
-    [
-        "nome" => "Bife",
-        "descricao" => "Bife, arroz com feijão e uma deliciosa batata frita",
-        "preco" => "27.90",
-        "imagem" => "img/bife.jpg"
-    ],
-    [
-        "nome" => "Filé de peixe",
-        "descricao" => "Filé de peixe salmão assado, arroz, feijão verde e tomate.",
-        "preco" => "24.99",
-        "imagem" => "img/prato-peixe.jpg"
-    ],
-    [
-        "nome" => "Frango",
-        "descricao" => "Saboroso frango à milanesa com batatas fritas, salada de repolho e molho picante",
-        "preco" => "23.00",
-        "imagem" => "img/prato-frango.jpg"
-    ],
-    [
-        "nome" => "Fettuccine",
-        "descricao" => "Prato italiano autêntico da massa do fettuccine com peito de frango grelhado",
-        "preco" => "22.50",
-        "imagem" => "img/fettuccine.jpg"
-    ]
-];
+require 'src/conexao-bd.php';
+
+$produtosCafe = $pdo->query("SELECT * FROM produtos WHERE tipo = 'cafe' ORDER BY preco")->fetchAll(PDO::FETCH_ASSOC);
+$produtosAlmoco = $pdo->query("SELECT * FROM produtos WHERE tipo = 'almoco' ORDER BY preco")->fetchAll(PDO::FETCH_ASSOC);
+
 ?>
 
 <!doctype html>
@@ -89,7 +42,7 @@ $produtosAlmoco = [
                 <?php foreach ($produtosCafe as $produto) : ?>
                     <div class="container-produto">
                         <div class="container-foto">
-                            <img src="<?= $produto['imagem']; ?>" alt="<?= $produto['nome']; ?>">
+                            <img src="img/<?= $produto['imagem']; ?>" alt="<?= $produto['nome']; ?>">
                         </div>
                         <p><?= $produto['nome']; ?></p>
                         <p><?= $produto['descricao']; ?></p>
@@ -107,7 +60,7 @@ $produtosAlmoco = [
                 <?php foreach ($produtosAlmoco as $produto) : ?>
                     <div class="container-produto">
                         <div class="container-foto">
-                            <img src="<?= $produto['imagem']; ?>" alt="<?= $produto['nome']; ?>">
+                            <img src="img/<?= $produto['imagem']; ?>" alt="<?= $produto['nome']; ?>">
                         </div>
                         <p><?= $produto['nome']; ?></p>
                         <p><?= $produto['descricao']; ?></p>
